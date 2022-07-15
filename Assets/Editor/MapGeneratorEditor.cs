abcbae68c2e831dbe075b70b7d1cb16a9e2ecc27
Wu <wujiaqu1233@gmail.com>
2022-07-15 04:22:51 +0200

    add some editor scripts

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        MapGenerator mapGenerator = (MapGenerator)target;
+        if(GUILayout.Button("Generator Map")) {
+            mapGenerator.GenerateNewMap();
+        }
+        
+        
+    }
+}
---
+++ Assets/Editor/QuadsManagerEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(QuadsManager))]
+public class QuadsManagerEditor : Editor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+        QuadsManager quadsManager = (QuadsManager)target;
+        if(GUILayout.Button("DeleteAllQuads")) {
+            quadsManager.DeleteAllQuads();
+        }
+
+    }
+}
