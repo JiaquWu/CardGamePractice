ceb28e19ed514c9d1efbc47d40515529ca279464
Wu <wujiaqu1233@gmail.com>
2022-07-18 23:00:55 +0200

    implement mouse enter and exit function to show emission effect

--- Assets/Scripts/Quad.cs
+++ Assets/Scripts/Quad.cs
@@ -3,8 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Quad : MonoBehaviour,IPointerEnterHandler {
-    public void OnPointerEnter(PointerEventData eventData) {
-        
+public class Quad : MonoBehaviour {
+    private string shaderTime = "Time_Current";
+    private GameObject emissionShader;
+    public GameObject EmissionShader {
+        get {
+            if(emissionShader == null) {
+                emissionShader = transform.Find("EmissionShader").gameObject;
+                if(emissionShader == null) {
+                    Debug.LogError("emission shader is missing");
+                }
+            }
+            return emissionShader;
+        }
+    }
+    private void Awake() {
+        EmissionShader.SetActive(false);
+    }
+    public void OnMouseEnter() {
+        emissionShader.GetComponent<Renderer>().material.SetFloat(shaderTime,Time.time);
+        EmissionShader.SetActive(true);
+    }
+    public void OnMouseExit() {
+        EmissionShader.SetActive(false);
     }
 }
