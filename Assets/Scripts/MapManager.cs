4d5a5a9db0ab9321ed19505350f15792389f23fa
Wu <wujiaqu1233@gmail.com>
2022-07-20 23:59:11 +0200

    add MapManager for accessing the current map configuration

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : SingletonManager<MapManager> {//这个东西管理和地图有关的所有东西,包括quads
+    [SerializeField]//目前还是在外部赋值吧
+    private MapConfigurationSO currentMapConfiguration;//只有生成地图的时候会给这个赋值
+    public MapConfigurationSO CurrentMapConfiguration {
+        get {
+            if(currentMapConfiguration == null) {
+                Debug.LogError("map configuration is missing!!");
+            }
+            return currentMapConfiguration;
+        }
+    }
+}
