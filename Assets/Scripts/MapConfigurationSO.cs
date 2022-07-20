ea9c6dd8e1611f41597e7ff1889e84f1e6f06231
Wu <wujiaqu1233@gmail.com>
2022-07-21 00:01:47 +0200

    now the origin point is at the bottom left edge of the first object

--- Assets/Scripts/MapConfigurationSO.cs
+++ Assets/Scripts/MapConfigurationSO.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Configurations/MapConfiguration",fileName ="MapConfiguration")]
 public class MapConfigurationSO : ScriptableObject {
-    private float scaleRatio = 1f;//坐标和实际坐标系的比率
     [SerializeField]
-    private Vector3 originPoint;//坐标轴原点,这里默认坐标轴的x轴和unity坐标x轴对应,y轴和unity坐标z轴对应
+    private float scaleRatio;//坐标和实际坐标系的比率
+    public float ScaleRatio => scaleRatio;
+    [SerializeField]
+    private int quadSizeX;//横向地图格数
+    public  int QuadSizeX => quadSizeX;
+    [SerializeField]
+    private int quadSizeY;//竖向地图格数
+    public int QuadSizeY => quadSizeY;
+    [SerializeField]
+    private Vector3 originPoint;//坐标轴原点,这里默认坐标轴的x轴和unity坐标x轴对应,y轴和unity坐标z轴对应,左下角顶点
     public Vector3 OriginPoint => originPoint;
     [SerializeField]
     private List<Vector2> preparationQuadCoordinates;//备战区坐标点
@@ -20,9 +28,9 @@ public class MapConfigurationSO : ScriptableObject {
 
     public Vector3 CalculatePosition(Vector2 coordinate) {
         Vector3 result;
-        result.x = originPoint.x + coordinate.x * scaleRatio;
+        result.x = originPoint.x + (coordinate.x + 0.5f) * scaleRatio;//+0.5是因为原点是第一格的左下角坐标,不应该是第一格中心坐标
         result.y = originPoint.y;
-        result.z = originPoint.z + coordinate.y * scaleRatio;
+        result.z = originPoint.z + (coordinate.y + 0.5f) * scaleRatio;
         return result;
     }
 
