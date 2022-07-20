c7baed756a44ba1f1ba61a23c6dcde15227d4dd8
Wu <wujiaqu1233@gmail.com>
2022-07-20 23:58:24 +0200

    add dictionary extension method

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extensions {
+    
+    public static Dictionary<TKey,TValue> MergeTwoDictionary<TKey,TValue>(this Dictionary<TKey,TValue> myDict,Dictionary<TKey,TValue> targetDict) {
+        foreach (var item in targetDict) {
+            if(!myDict.ContainsKey(item.Key)) {
+                myDict.Add(item.Key,item.Value);
+            }
+        }
+        return myDict;
+    }
+}
