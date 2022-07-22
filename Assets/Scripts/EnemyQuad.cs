9036e3e876d41b9ca6dd86ccc1efa7fdb08d7683
Wu <wujiaqu1233@gmail.com>
2022-07-22 17:43:41 +0200

    made node initialize function

--- Assets/Scripts/EnemyQuad.cs
+++ Assets/Scripts/EnemyQuad.cs
@@ -3,5 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyQuad : Quad {
-    
+    public override void InitializeNode(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY) {
+        node = new Node(_walkable,_worldPos,_gridX,_gridY-1);//-1是因为生成map的坐标和算法相关不一样
+    }
 }
