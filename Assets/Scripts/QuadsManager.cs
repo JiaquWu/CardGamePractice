2a9777da8066fdf15d7ddd634de3eda8732c7d95
Wu <wujiaqu1233@gmail.com>
2022-07-26 03:37:50 +0200

    make some calculation happen in map's function not in manager's function

--- Assets/Scripts/QuadsManager.cs
+++ Assets/Scripts/QuadsManager.cs
@@ -54,9 +54,9 @@ public class QuadsManager : SingletonManager<QuadsManager> {
     private int quadSizeX;//这个是x轴总共能放多少个quad
     private int quadSizeY;
     public int MaxSize => quadSizeX * quadSizeY;
-    Vector2 gridWorldSize;//这个是说坐标系的X轴有多宽
+    Vector2 gridWorldSize;//和寻路有关的区域面积大小
     float nodeRadius;
-    Node[,] grid;//所有跟寻路有关的node,(0,0)是地图里面的(1,0),因为不包括preparation
+    Node[,] grid;//所有跟寻路有关的node,(0,0)是地图里面的(1,0),因为不包括preparation,这个转换是地图自己做
     public Transform start;
     public Transform end;
     private void OnDrawGizmos() {
@@ -74,24 +74,27 @@ public class QuadsManager : SingletonManager<QuadsManager> {
     }
     protected override void Init() {
         InitializeAllQuads();
+        InitializeGrid();
+    }
+    private void Update() {
+        FindPath(start.position,end.position);
+    }
+    private void InitializeGrid() {
         MapConfigurationSO map = MapManager.Instance.CurrentMapConfiguration;
         quadSizeX = map.QuadSizeX;
-        quadSizeY = map.QuadSizeY-1;//-1是因为quadSize是9
+        quadSizeY = map.QuadSizeY;
         grid = new Node[quadSizeX,quadSizeY];
         gridWorldSize = new Vector2(quadSizeX * map.ScaleRatio,quadSizeY * map.ScaleRatio);
         for (int x = 0; x < quadSizeX; x++) {
             for (int y = 0; y < quadSizeY; y++) {
-                if(findPathDict.ContainsKey(new Vector2(x,y+1))) {//y+1是因为不包括preparation区域
-                    grid[x,y] = findPathDict[new Vector2(x,y+1)].node;
+                Vector2 coordinate = map.AStarCoordinateToCoordinate(new Vector2(x,y));
+                if(findPathDict.ContainsKey(coordinate)) {
+                    grid[x,y] = findPathDict[coordinate].node;
                 }else {
                     UnityEngine.Debug.LogWarning("key is missing!");//目前编辑器中生成地图也会执行到这里,目前不用管
                 }
             }            
         }
-        
-    }
-    private void Update() {
-        FindPath(start.position,end.position);
     }
     private void InitializeAllQuads() {    
         RegisterQuads(DeployQuads,deployQuadsDict,MapManager.Instance.CurrentMapConfiguration.DeployQuadCoordinates);
@@ -114,9 +117,10 @@ public class QuadsManager : SingletonManager<QuadsManager> {
         } 
     }
     public Node NodeFromWorldPoint(Vector3 worldPosition) {
-        float percentX = (worldPosition.x - MapManager.Instance.CurrentMapConfiguration.OriginPoint.x) / gridWorldSize.x;
-        float percentY = (worldPosition.z - (MapManager.Instance.CurrentMapConfiguration.OriginPoint.z+1)) / gridWorldSize.y; //+1因为不算preparation
-        percentX = Mathf.Clamp01(percentX);
+        MapConfigurationSO map = MapManager.Instance.CurrentMapConfiguration;
+        Vector2 offset = map.GetOffset();//寻路的原点和所有点坐标原点的偏移量
+        float percentX = (worldPosition.x - (MapManager.Instance.CurrentMapConfiguration.OriginPoint.x + offset.x)) / gridWorldSize.x;
+        float percentY = (worldPosition.z - (MapManager.Instance.CurrentMapConfiguration.OriginPoint.z + offset.y)) / gridWorldSize.y; 
         percentY = Mathf.Clamp01(percentY);
         int x = Mathf.CeilToInt(quadSizeX * percentX) - 1;//这里是推算出来的要ceil,先算出百分比,ceil是因为最大7.5,7.0到7.5都是第八格,然后因为index要-1
         int y = Mathf.CeilToInt(quadSizeY * percentY) - 1;//同理,因为这里quadSizeY是8,所以一样的
