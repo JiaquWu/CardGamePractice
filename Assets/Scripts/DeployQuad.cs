b371564dcebf7c01e72ea4a06b21951688f6b8ff
Wu <wujiaqu1233@gmail.com>
2022-07-27 01:35:59 +0200

    add some explanation

--- Assets/Scripts/DeployQuad.cs
+++ Assets/Scripts/DeployQuad.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class DeployQuad : Quad {
     public override void InitializeNode(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY) {
-        node = new Node(_walkable,_worldPos,_gridX,_gridY-1);//-1是因为生成map的坐标和算法相关不一样
+        node = new Node(_walkable,_worldPos,_gridX,_gridY-1);//-1是因为生成map的坐标和算法相关不一样,这里也是只符合这张地图
     }
 }
