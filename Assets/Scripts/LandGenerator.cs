77409092ba86082202c7a01fe36f2f0f93f989dd
Wu <wujiaqu1233@gmail.com>
2022-07-31 00:30:16 +0200

    set width and height of the map to 241, so it's easier to implement LOD, also it's implemented

--- Assets/Scripts/LandGenerator.cs
+++ Assets/Scripts/LandGenerator.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class LandGenerator : MonoBehaviour {
     public enum DrawMode {NoiseMap, colorMap,Mesh}
     public DrawMode drawMode;
-    public int mapWidth;
-    public int mapHeight;
+    const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int levelOfDetail;//2,4,6,8,10,12,这些都能被241-1整除
     public float noiseScale;
 
     public int octaves;
@@ -16,18 +17,20 @@ public class LandGenerator : MonoBehaviour {
     
     public int seed;
     public Vector2 offset;
+    public float meshHeightMultiplier;
+    public AnimationCurve meishHeightCurve;
     public bool autoUpdate;
     public TerrianType[] regions;
     //生成顺序：先由noise类的方法得到一张float[,]类型的噪点图，也就是heightMap，然后噪点图经过色彩处理要么变成黑白的texture，要么变成彩色的texture
     public void GenerateMap() {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth,mapHeight,seed,noiseScale,octaves,persistance,lacunarity,offset);
-        Color[] colorMap = new Color[mapHeight * mapWidth];
-        for (int y = 0; y < mapHeight; y++) {
-            for (int x = 0; x < mapWidth; x++) {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize,mapChunkSize,seed,noiseScale,octaves,persistance,lacunarity,offset);
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++) {
+            for (int x = 0; x < mapChunkSize; x++) {
                 float currentHeight = noiseMap[x,y];
                 for (int i = 0; i < regions.Length; i++) {
                     if(currentHeight <= regions[i].height) {
-                        colorMap[y*mapWidth + x] = regions[i].color;
+                        colorMap[y*mapChunkSize + x] = regions[i].color;
                         break;
                     }
                 }
@@ -37,19 +40,13 @@ public class LandGenerator : MonoBehaviour {
         if(drawMode == DrawMode.NoiseMap) {//如果只是得到噪点图，那就是黑白的，
             display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
         }else if(drawMode == DrawMode.colorMap) {//如果要加颜色，那么就是这个脚本开放变量中设置的颜色+
-            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap,mapWidth,mapHeight));
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap,mapChunkSize,mapChunkSize));
         }else if (drawMode == DrawMode.Mesh) {//如果是要生成mesh
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap),TextureGenerator.TextureFromColorMap(colorMap,mapWidth,mapHeight));
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap,meshHeightMultiplier,meishHeightCurve,levelOfDetail),TextureGenerator.TextureFromColorMap(colorMap,mapChunkSize,mapChunkSize));
         }
         
     }
     private void OnValidate() {
-        if(mapWidth < 1) {
-            mapWidth = 1;
-        }
-        if(mapHeight < 1) {
-            mapHeight = 1;
-        }
         if(lacunarity < 1) {
             lacunarity = 1;
         }
--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class MeshGenerator {
-    public static MeshData GenerateTerrainMesh(float[,] heightMap) {
+    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier,AnimationCurve heightCurve,int levelOfDetail) {//生成新的meshData，里面包括生成mesh所需的顶点，三角，UV
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
         float topLeftX = (width-1)/-2f;//比如横向总共三个点，那么第二点为中心点，第一点的横坐标就是-1
         float topLeftZ = (height-1)/2f;//从左上角开始
-        MeshData meshData = new MeshData(width,height);
+        
+        int MeshSimplificationIncrement = levelOfDetail == 0 ? 1 : levelOfDetail * 2;//为0就无法继续增加了
+        int verticesPerLine = (width-1)/MeshSimplificationIncrement + 1;
+        MeshData meshData = new MeshData(verticesPerLine,verticesPerLine);
         int vertexIndex = 0;
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                meshData.vertices[vertexIndex] = new Vector3(topLeftX + x,heightMap[x,y],topLeftZ - y);
+        for (int y = 0; y < height; y+=MeshSimplificationIncrement) {//改用这个参数，从而能减少vertex，这便是LOD(level of detail)
+            for (int x = 0; x < width; x+=MeshSimplificationIncrement) {
+                meshData.vertices[vertexIndex] = new Vector3(topLeftX + x,heightCurve.Evaluate(heightMap[x,y])  * heightMultiplier,topLeftZ - y);
                 meshData.uvs[vertexIndex] = new Vector2(x/(float)width,y/(float)height);//让uvs里面每个值是这个点在整张图中的相对位置
                 if(x < width -1 && y < height - 1) {//意味着不是右边和下面的边界
-                    meshData.AddTriangle(vertexIndex,vertexIndex + width + 1,vertexIndex + width);//顺时针三个点
-                    meshData.AddTriangle(vertexIndex,vertexIndex + 1,vertexIndex + width + 1);
+                    meshData.AddTriangle(vertexIndex,vertexIndex + verticesPerLine + 1,vertexIndex + verticesPerLine);//顺时针三个点
+                    meshData.AddTriangle(vertexIndex,vertexIndex + 1,vertexIndex + verticesPerLine + 1);
                 }  
                 vertexIndex ++;
             }
