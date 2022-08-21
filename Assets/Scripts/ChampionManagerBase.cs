708964197a577dc94234d98a557c8e12594ef3dc
Wu <wujiaqu1233@gmail.com>
2022-08-21 21:39:01 +0200

    add champion manager script

---
+++ Assets/Scripts/ChampionManagerBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//管理基类,友方和敌方不太一样,但是大部分方法应该是通用的
+public class ChampionManagerBase : SingletonManager<ChampionManagerBase> {
+    protected Dictionary<string,List<Champion>> championsDict;
+    protected override void Init() {
+        championsDict = new Dictionary<string, List<Champion>>();
+    }
+
+    public static void RegisterChampion(Champion champion) {
+        if(Instance.championsDict.ContainsKey(champion.ChampionName)) {
+            Instance.championsDict[champion.ChampionName].Add(champion);
+        }else {
+            Instance.championsDict.Add(champion.ChampionName,new List<Champion>());
+        }
+    }
+
+    public static void UnregisterChampion(Champion champion) {
+        if(!Instance.championsDict.ContainsKey(champion.ChampionName)) {
+            Debug.LogError("no champion instance in this dictionary");
+        }else if(Instance.championsDict[champion.ChampionName].Count != 0){
+            Instance.championsDict[champion.ChampionName].Remove(champion);
+        }
+    }
+}
