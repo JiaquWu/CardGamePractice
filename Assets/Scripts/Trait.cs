1a8e93392409ea6e47a88825970b1c5c60428403
Wu <wujiaqu1233@gmail.com>
2022-08-21 21:41:55 +0200

    add some logic stuff for activating trait

--- Assets/Scripts/Trait.cs
+++ Assets/Scripts/Trait.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,15 +7,20 @@ using UnityEngine;
 public class TraitBase  {//羁绊的基类
     public virtual string TriatName{get;protected set;} //羁绊名字
     protected virtual int ActivatedTraitsIndex {get; set;} = -1;//0就是激活了第一级羁绊
-    protected  int[] amountToActivate;//激活羁绊所需要的英雄数量
+    protected Action<int> ActivateAdditionalEffect;//int是说第几级,英雄知道每级效果会给自己什么不同的额外效果.0是1级
+    protected int[] amountToActivate;//激活羁绊所需要的英雄数量
+    public TraitBase(Action<int> ActivateAdditionalEffect) {//创建对象的时候,把
+        this.ActivateAdditionalEffect = ActivateAdditionalEffect;
+        Init();
+    }
     public virtual Champion ActivateTrait(int championAmount, Champion champion) {//激活羁绊会发生的事
         return champion;
     }
     public virtual void AdditionalEffect() {//一些特殊能力
 
     }
-    public virtual void Init() {
-
+    public virtual void Init() {//构造对象时需要初始化的东西
+        amountToActivate = new int[]{};
     }
     protected virtual int CalculateNewIndex(int amount) {//计算英雄数量所指向的新的index是几
         int result = -1;
@@ -30,7 +36,9 @@ public class TraitBase  {//羁绊的基类
 
 public class TestTrait: TraitBase {
     public override string TriatName { get; protected set; } = "shimmerscale";
-
+    public TestTrait(Action<int> action):base(action) {
+        
+    } 
     public override void Init() {
         amountToActivate = new int[]{3,5,7,9};
     }
@@ -40,8 +48,13 @@ public class TestTrait: TraitBase {
     }
     public override Champion ActivateTrait(int championAmount, Champion champion) {
         //首先要判断是否需要激活,如果当前的index和championAmount所指向的是同一个index,那么就直接return;
-        if(ActivatedTraitsIndex == CalculateNewIndex(championAmount)) return champion; 
+        if(ActivatedTraitsIndex == CalculateNewIndex(championAmount)) {
+            return champion; 
+        }else {
+            ActivatedTraitsIndex = CalculateNewIndex(championAmount);
+        } 
         champion.currentChampionStats.attackDamage += 1;
+        ActivateAdditionalEffect?.Invoke(ActivatedTraitsIndex);
         return champion;
     }
 }
