25fcd29f9d0c85f6786f22096c1769d2e48e1235
Wu <wujiaqu1233@gmail.com>
2022-08-23 02:26:22 +0200

    add parameter to initialize func

--- Assets/Scripts/DeployQuad.cs
+++ Assets/Scripts/DeployQuad.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DeployQuad : Quad {
-    public override void InitializeNode(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY) {
-        node = new Node(_walkable,_worldPos,_gridX,_gridY-1);//-1是因为生成map的坐标和算法相关不一样,这里也是只符合这张地图
+    public override void InitializeNode(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY, Quad _attachedQuad) {
+        node = new Node(_walkable,_worldPos,_gridX,_gridY-1,_attachedQuad);//-1是因为生成map的坐标和算法相关不一样,这里也是只符合这张地图
     }
 }
