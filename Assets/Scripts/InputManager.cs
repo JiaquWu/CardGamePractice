2d933140f8e6021084790ddc9924b77c325c39e0
Wu <wujiaqu1233@gmail.com>
2022-08-23 02:23:49 +0200

    add input manager script

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : SingletonManager<InputManager> {
+    public bool IsLeftMouseButtonPressed => Input.GetMouseButton(0); //鼠标左键有没有按下
+}
