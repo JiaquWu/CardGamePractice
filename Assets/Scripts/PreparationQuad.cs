149a073a82ca8b6c918fc155f7c3ca7a86800eb2
Wu <wujiaqu1233@gmail.com>
2022-08-23 02:25:05 +0200

    add initialize node func

--- Assets/Scripts/PreparationQuad.cs
+++ Assets/Scripts/PreparationQuad.cs
@@ -3,5 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PreparationQuad : Quad {
-    
+    public override void InitializeNode(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY, Quad _attachedQuad) {
+        node = new Node(_worldPos,_attachedQuad);
+    }
 }
