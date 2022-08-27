c65e14b134ab2be5ef6e9e231d40df14ed10b3fb
Wu <wujiaqu1233@gmail.com>
2022-08-27 19:12:20 +0200

    add player class for checking the money for now

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : SingletonManager<Player> {
+    private int money;
+    public int Money => money;
+
+
+
+
+    protected override void Init() {
+        GameEventsManager.StartListening(GameEventTypeVoid.ENTER_INIT_STATE,OnEnterInitState);
+    }
+    private void OnEnterInitState(GameEventTypeVoid ev) {
+        money = 100;//游戏初始化,那么金币清零
+    }
+    private void OnDisable() {
+        GameEventsManager.StopListening(GameEventTypeVoid.ENTER_INIT_STATE,OnEnterInitState);
+    }   
+}
