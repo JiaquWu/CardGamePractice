4b5b386b30bb47cb33614287dab22f696b9038ed
Wu <wujiaqu1233@gmail.com>
2022-08-27 19:13:32 +0200

    add onpointclick func for buying a champion

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,8 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour {//和抽卡有关的事情都在这里
-    public int cost;//购买所需费用
-    
+public class Card :MonoBehaviour,IPointerClickHandler {//和抽卡有关的事情都在这里
+    [SerializeField]
+    private int cost;//购买所需费用
+    [SerializeField]
+    private GameObject championPrefab;//这张卡代表的英雄,应该还是要手动去配好
+    public void OnPointerClick(PointerEventData eventData) {
+        if(Player.Instance.Money >= cost) {
+            //应该告诉一个英雄生成器之类的生成英雄
+            ChampionGenerator.Instance.GenerateChampion(championPrefab);
+        }
+    }
 }
