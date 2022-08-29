8d680896b9495038ee638f1ae2cfd0337e16cc6b
Wu <wujiaqu1233@gmail.com>
2022-08-29 20:52:05 +0200

    add sell champion button func

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,7 +1,13 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : SingletonManager<InputManager> {
     public bool IsLeftMouseButtonPressed => Input.GetMouseButton(0); //鼠标左键有没有按下
+    private void Update() {
+        if(Input.GetButtonDown("Sell")) {
+            Debug.Log("sell");
+            GameEventsManager.TriggerEvent(GameEventTypeVoid.ON_SELL_BUTTON_DOWN);
+        }
+    }
 }
