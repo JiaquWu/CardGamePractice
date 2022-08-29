eff66b1975ab4e9b46664fc9683f17361707d538
Wu <wujiaqu1233@gmail.com>
2022-08-29 17:28:03 +0200

    add onsell and onbuy func to change the money, also listenning to the events

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,11 +11,27 @@ public class Player : SingletonManager<Player> {
 
     protected override void Init() {
         GameEventsManager.StartListening(GameEventTypeVoid.ENTER_INIT_STATE,OnEnterInitState);
+        GameEventsManager.StartListening(GameEventTypeGameObject.BUY_A_CHAMPION,OnBuyAChampion);
+        GameEventsManager.StartListening(GameEventTypeGameObject.SELL_A_CHAMPION,OnSellAChampion);
+        
     }
     private void OnEnterInitState(GameEventTypeVoid ev) {
         money = 100;//游戏初始化,那么金币清零
     }
+    private void OnBuyAChampion(GameEventTypeGameObject ev,GameObject go) {
+        Debug.Log("OnBuyAChampion");
+        if(go.TryGetComponent<Champion>(out Champion _champion)) {
+           money -= _champion.Cost;
+        }
+    }
+    private void OnSellAChampion(GameEventTypeGameObject ev,GameObject go) {
+        if(go.TryGetComponent<Champion>(out Champion _champion)) {
+           money += _champion.Cost;
+        }
+    }
     private void OnDisable() {
         GameEventsManager.StopListening(GameEventTypeVoid.ENTER_INIT_STATE,OnEnterInitState);
+        GameEventsManager.StopListening(GameEventTypeGameObject.BUY_A_CHAMPION,OnBuyAChampion);
+        GameEventsManager.StopListening(GameEventTypeGameObject.SELL_A_CHAMPION,OnSellAChampion);
     }   
 }
