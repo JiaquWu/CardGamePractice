224f3292255b1566161d15bf1ba4cbf0eba5ec82
Wu <wujiaqu1233@gmail.com>
2022-08-30 19:44:14 +0200

    change champion dict init func to init it in a more stable way,and implement champion upgrade feature in register func

--- Assets/Scripts/ChampionManagerBase.cs
+++ Assets/Scripts/ChampionManagerBase.cs
@@ -1,27 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 //管理基类,友方和敌方不太一样,但是大部分方法应该是通用的
 public class ChampionManagerBase : SingletonManager<ChampionManagerBase> {
-    protected Dictionary<string,List<Champion>> championsDict;
-    protected override void Init() {
-        championsDict = new Dictionary<string, List<Champion>>();
-    }
-
-    public static void RegisterChampion(Champion champion) {
-        if(Instance.championsDict.ContainsKey(champion.ChampionName)) {
-            Instance.championsDict[champion.ChampionName].Add(champion);
-        }else {
-            Instance.championsDict.Add(champion.ChampionName,new List<Champion>());
+    protected static Dictionary<string,List<Champion>> championsDict;
+    static void InitDict() {
+        if(championsDict == null) {
+            championsDict = new Dictionary<string, List<Champion>>();
         }
     }
-
+    public static void RegisterChampion(Champion champion) {
+        InitDict();
+        if(!championsDict.ContainsKey(champion.ChampionName)) {
+            championsDict.Add(champion.ChampionName,new List<Champion>());
+        }
+        championsDict[champion.ChampionName].Add(champion);
+        //应该需要遍历一遍这个list,如果有三个0级的,就让第一个0级的callback(1),其他两个callback(0)
+        //如果有三个0级的并且有两个1级的,就让第一个1级的callback(2),其余的全部callback(0)
+        if(championsDict[champion.ChampionName].Count <= 2) return;
+        IEnumerable<Champion> championLevel0IE =  
+        from item in championsDict[champion.ChampionName]
+        where item.Level == 0
+        select item;
+        List<Champion> championLevel0 = championLevel0IE.ToList();
+        IEnumerable<Champion> championLevel1IE = 
+        from item in championsDict[champion.ChampionName]
+        where item.Level == 1
+        select item;
+        List<Champion> championLevel1 = championLevel1IE.ToList();
+        if(championLevel0.Count >= 3) {
+            championLevel0[0].OnChampionUpgrade(1);
+            championLevel0[1].OnChampionUpgrade(0);
+            championLevel0[2].OnChampionUpgrade(0);
+            championLevel1.Add(championLevel0[0]);
+            GameEventsManager.TriggerEvent(GameEventTypeInt.CHAMPION_UPGRADE,0);
+        }
+        if(championLevel1.Count >= 3) {
+            championLevel1[0].OnChampionUpgrade(2);
+            championLevel1[1].OnChampionUpgrade(0);
+            championLevel1[2].OnChampionUpgrade(0);
+            GameEventsManager.TriggerEvent(GameEventTypeInt.CHAMPION_UPGRADE,2);
+        }
+    }
     public static void UnregisterChampion(Champion champion) {
-        if(!Instance.championsDict.ContainsKey(champion.ChampionName)) {
+        InitDict();
+        if(!championsDict.ContainsKey(champion.ChampionName)) {
             Debug.LogError("no champion instance in this dictionary");
-        }else if(Instance.championsDict[champion.ChampionName].Count != 0){
-            Instance.championsDict[champion.ChampionName].Remove(champion);
+        }else if(championsDict[champion.ChampionName].Count != 0){
+            championsDict[champion.ChampionName].Remove(champion);
         }
     }
 }
