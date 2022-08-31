f7c5c2e092851d9b2571a5eb6256825e7f8580ee
Wu <wujiaqu1233@gmail.com>
2022-08-31 20:59:59 +0200

    add uimanager and menubase script

---
+++ Assets/Scripts/MenuBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBase : MonoBehaviour {
+    // protected virtual void Awake() {
+        
+    // }
+    // protected virtual void OnInit() {
+
+    // }
+    // protected virtual void OnExit() {
+
+    // }
+    // protected virtual void OnDisable() {
+        
+    // }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Menu {
+
+}
+public class UIManager : SingletonManager<UIManager> {
+    static Dictionary<Menu,GameObject> menus;
+
+    void InitDict() {
+        if(menus == null) {
+            menus = new Dictionary<Menu, GameObject>();
+        }
+    }
+}
