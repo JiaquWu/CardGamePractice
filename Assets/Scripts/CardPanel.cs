26dfa09cc9026bdcb0dab6276c083b1951e55f4a
Wu <wujiaqu1233@gmail.com>
2022-09-01 23:00:23 +0200

    add cardpanel script and panelbase script

---
+++ Assets/Scripts/CardPanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPanel : PanelBase {
+    void OnDisable() {
+        GameEventsManager.StopListening(GameEventTypeVoid.ON_REFRESH_BUTTON_DOWN,OnRefreshButtonDown);
+    }
+
+    private void OnEnable() {
+        BaseOnLoad();
+        GameEventsManager.StartListening(GameEventTypeVoid.ON_REFRESH_BUTTON_DOWN,OnRefreshButtonDown);
+    }
+
+    void OnRefreshButtonDown(GameEventTypeVoid ev) {
+        Debug.Log("refresh!!!!");
+    }
+}
---
+++ Assets/Scripts/PanelBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelBase : MonoBehaviour {
+    // protected virtual void Awake() {
+        
+    // }
+    // protected virtual void OnInit() {
+
+    // }
+    // protected virtual void OnExit() {
+
+    // }
+    // protected virtual void OnDisable() {
+        
+    // }
+    public CanvasGroup canvasGroup;
+    public void BaseOnLoad() {
+        canvasGroup = GetComponent<CanvasGroup>();
+        if(canvasGroup == null) {
+            canvasGroup = gameObject.AddComponent<CanvasGroup>();
+        }
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.interactable = true;
+        canvasGroup.alpha = 1;
+    }
+}
