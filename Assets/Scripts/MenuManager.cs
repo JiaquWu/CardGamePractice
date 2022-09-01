f6f54b85d020bb1ca55f491c405a43559227ef7a
Wu <wujiaqu1233@gmail.com>
2022-09-01 23:00:47 +0200

    add menumanager script for future use

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Menu {
+    
+}
+public class MenuManager : SingletonManager<MenuManager> {
+//先不用,后面如果有暂停菜单那种才会用到
+    public static Dictionary<Menu,GameObject> menus;
+    protected override void Init() {
+        menus = new Dictionary<Menu, GameObject>() {
+
+        };
+        foreach (var menu in menus.Values) {
+            menu.SetActive(false);
+        }
+    }
+}
