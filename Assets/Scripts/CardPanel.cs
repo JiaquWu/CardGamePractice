617eb4ac9c28e5813326dccd4d76edc24d92ce23
Wu <wujiaqu1233@gmail.com>
2022-09-03 20:42:20 +0200

    add refresh card panel func to refresh every button in its children objects

--- Assets/Scripts/CardPanel.cs
+++ Assets/Scripts/CardPanel.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CardPanel : PanelBase {
+    List<CardButton> cardButtons = new List<CardButton>();
     void OnDisable() {
         GameEventsManager.StopListening(GameEventTypeVoid.ON_REFRESH_BUTTON_DOWN,OnRefreshButtonDown);
     }
 
     private void OnEnable() {
-        BaseOnLoad();
+        cardButtons = GetComponentsInChildren<CardButton>(true).ToList<CardButton>();
+        Debug.Log(cardButtons.Count);
         GameEventsManager.StartListening(GameEventTypeVoid.ON_REFRESH_BUTTON_DOWN,OnRefreshButtonDown);
+        BaseOnLoad();
+        RefreshCardPanel();
+        
     }
 
     void OnRefreshButtonDown(GameEventTypeVoid ev) {
+        RefreshCardPanel();
         Debug.Log("refresh!!!!");
+    }
+    void RefreshCardPanel() {
+        if(cardButtons.Count == 0) return;
+        foreach (var item in cardButtons) {
+            item.OnRefresh(CardManager.GenerateRandomCard());
+        }
     }
 }
