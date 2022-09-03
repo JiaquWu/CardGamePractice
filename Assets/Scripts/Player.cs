fcaeaa25aef92f7a312629288bda89bf459be374
Wu <wujiaqu1233@gmail.com>
2022-09-03 20:44:07 +0200

    add player level up func, not implement triggering it yet, add list for storing the current champion drop rate

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,21 @@ public class Player : SingletonManager<Player> {
     private int money;
     public int Money => money;
 
-
-
+    private List<float> currentChampionDropRate;
+    public List<float> CurrentChampionDropRate => currentChampionDropRate;
+    int currentLevel;
 
     protected override void Init() {
-        GameEventsManager.StartListening(GameEventTypeVoid.ENTER_INIT_STATE,OnEnterInitState);
+        GameEventsManager.StartListening(GameEventTypeVoid.ENTER_PLAY_STATE,OnEnterPlayState);
         GameEventsManager.StartListening(GameEventTypeGameObject.BUY_A_CHAMPION,OnBuyAChampion);
         GameEventsManager.StartListening(GameEventTypeGameObject.SELL_A_CHAMPION,OnSellAChampion);
+        GameEventsManager.StartListening(GameEventTypeInt.LEVEL_UP,OnLevelUp);
         
     }
-    private void OnEnterInitState(GameEventTypeVoid ev) {
+    private void OnEnterPlayState(GameEventTypeVoid ev) {
         money = 100;//游戏初始化,那么金币清零
+        currentLevel = 1;
+        currentChampionDropRate = GameRulesManager.championDropRatesByLevel[1];
     }
     private void OnBuyAChampion(GameEventTypeGameObject ev,GameObject go) {
         Debug.Log("OnBuyAChampion");
@@ -29,9 +33,15 @@ public class Player : SingletonManager<Player> {
            money += _champion.Cost;
         }
     }
+    private void OnLevelUp(GameEventTypeInt ev,int targetLevel) {
+        if(GameRulesManager.championDropRatesByLevel.ContainsKey(targetLevel)) {
+            currentChampionDropRate = GameRulesManager.championDropRatesByLevel[targetLevel];
+        }
+    }
     private void OnDisable() {
-        GameEventsManager.StopListening(GameEventTypeVoid.ENTER_INIT_STATE,OnEnterInitState);
+        GameEventsManager.StopListening(GameEventTypeVoid.ENTER_PLAY_STATE,OnEnterPlayState);
         GameEventsManager.StopListening(GameEventTypeGameObject.BUY_A_CHAMPION,OnBuyAChampion);
         GameEventsManager.StopListening(GameEventTypeGameObject.SELL_A_CHAMPION,OnSellAChampion);
+        GameEventsManager.StopListening(GameEventTypeInt.LEVEL_UP,OnLevelUp);
     }   
 }
