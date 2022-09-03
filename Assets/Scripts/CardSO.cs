7af5714639075c106214189900f38fad224b244e
Wu <wujiaqu1233@gmail.com>
2022-09-03 20:36:23 +0200

    add cardSO script

---
+++ Assets/Scripts/CardSO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Cards/Card",fileName = "Card")]
+public class CardSO : ScriptableObject {
+    
+    [SerializeField]
+    private int cost;//购买所需费用
+    public int Cost => cost;
+    [SerializeField]
+    private Sprite cardSprite;
+    public Sprite CardSprite => cardSprite;
+    [SerializeField]
+    private GameObject championPrefab;//这张卡代表的英雄,应该还是要手动去配好
+    public GameObject ChampionPrefab => championPrefab;
+}
