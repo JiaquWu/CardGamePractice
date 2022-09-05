4c4a803e4c70b28bf3200c5acdb4f55ea252472c
Wu <wujiaqu1233@gmail.com>
2022-09-05 18:58:24 +0200

    add references for gold,exp,level text, add func to update these texts, add updating event related code

--- Assets/Scripts/CardPanel.cs
+++ Assets/Scripts/CardPanel.cs
@@ -2,23 +2,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+//需要手动在unity中关闭
 public class CardPanel : PanelBase {
     List<CardButton> cardButtons = new List<CardButton>();
-    void OnDisable() {
-        GameEventsManager.StopListening(GameEventTypeVoid.ON_REFRESH_BUTTON_DOWN,OnRefreshButtonDown);
-    }
-
+    [SerializeField]
+    private Text goldText;
+    [SerializeField]
+    private Text levelText;
+    [SerializeField]
+    private Text expText;
     private void OnEnable() {
         cardButtons = GetComponentsInChildren<CardButton>(true).ToList<CardButton>();
         Debug.Log(cardButtons.Count);
-        GameEventsManager.StartListening(GameEventTypeVoid.ON_REFRESH_BUTTON_DOWN,OnRefreshButtonDown);
+        GameEventsManager.StartListening(GameEventTypeVoid.EXECUTE_REFRESH,OnRefreshExecute);
+        GameEventsManager.StartListening(GameEventTypeInt.UPDATE_MONEY,UpdateGoldText);
+        GameEventsManager.StartListening(GameEventTypeInt.UPDATE_LEVEL,UpdateLevelText);
+        GameEventsManager.StartListening(GameEventTypeInt.UPDATE_EXP,UpdateExpText);
         BaseOnLoad();
         RefreshCardPanel();
-        
+        GameEventsManager.TriggerEvent(GameEventTypeInt.UPDATE_MONEY,Player.Instance.Money);//这三个需要初始化刷新
+        GameEventsManager.TriggerEvent(GameEventTypeInt.UPDATE_EXP,Player.Instance.CurrentExp);
+        GameEventsManager.TriggerEvent(GameEventTypeInt.UPDATE_LEVEL,Player.Instance.CurrentLevel);
     }
-
-    void OnRefreshButtonDown(GameEventTypeVoid ev) {
+    void OnDisable() {
+        GameEventsManager.StopListening(GameEventTypeVoid.EXECUTE_REFRESH,OnRefreshExecute);
+        GameEventsManager.StopListening(GameEventTypeInt.UPDATE_MONEY,UpdateGoldText);
+        GameEventsManager.StopListening(GameEventTypeInt.UPDATE_LEVEL,UpdateLevelText);
+        GameEventsManager.StopListening(GameEventTypeInt.UPDATE_EXP,UpdateExpText);
+    }
+    void OnRefreshExecute(GameEventTypeVoid ev) {
         RefreshCardPanel();
         Debug.Log("refresh!!!!");
     }
@@ -26,6 +40,23 @@ public class CardPanel : PanelBase {
         if(cardButtons.Count == 0) return;
         foreach (var item in cardButtons) {
             item.OnRefresh(CardManager.GenerateRandomCard());
+        }
+    }
+    void UpdateGoldText(GameEventTypeInt ev,int goldAmount) {
+        Debug.Log("???????");
+        if(goldText != null) {
+            goldText.text = "Gold : " + goldAmount;
+        }
+    }
+    void UpdateLevelText(GameEventTypeInt ev,int currentLevel) {
+        if(levelText != null) {
+            levelText.text = "Level : " + currentLevel;
+        }
+    }
+    void UpdateExpText(GameEventTypeInt ev,int currentExp) {
+        if(expText != null) {
+            expText.text = "Exp : " + currentExp + " / " + GameRulesManager.experienceRequirementByLevel[Player.Instance.CurrentLevel];
+            //这里没有检测后面字典是否能找到是因为触发这个函数的代码有检测，但是这样还是不是很好
         }
     }
 }
