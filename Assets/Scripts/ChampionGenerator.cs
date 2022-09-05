fd02d9a516f500f22078267923f8cea6ada7e02a
Wu <wujiaqu1233@gmail.com>
2022-09-05 02:04:53 +0200

    revise some name

--- Assets/Scripts/ChampionGenerator.cs
+++ Assets/Scripts/ChampionGenerator.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 using System.Linq;
 public class ChampionGenerator : SingletonManager<ChampionGenerator> {
     protected override void Init() {
-        GameEventsManager.StartListening(GameEventTypeGameObject.BUY_A_CHAMPION,GenerateChampion);
+        GameEventsManager.StartListening(GameEventTypeChampion.BUY_A_CHAMPION,GenerateChampion);
     }
-    public void GenerateChampion(GameEventTypeGameObject ev,GameObject championPrefab) {
+    public void GenerateChampion(GameEventTypeChampion ev,Champion _champion) {
         //考虑要不要加限制,目前调试阶段不用,之后写好逻辑应该也还好
         Dictionary<Vector2,Quad> dict = QuadsManager.Instance.preparationQuadsDict;
         for (int i = 0; i < dict.Count; i++) {
             if(dict.ElementAt(i).Value.ChampionOnThisQuad == null) {
                 //说明可以在这里生成
                 Vector3 pos = dict.ElementAt(i).Value.node.worldPosition;
-                GameObject go = Instantiate(championPrefab,pos,Quaternion.identity);
+                GameObject go = Instantiate(_champion.gameObject,pos,Quaternion.identity);
                 if(go.TryGetComponent<Champion>(out Champion champion)) {
                     champion.OnDeploy(dict.ElementAt(i).Value);
                 }
@@ -24,6 +24,6 @@ public class ChampionGenerator : SingletonManager<ChampionGenerator> {
         Debug.LogWarning("no place for instantiating a new champion");
     }
     private void OnDisable() {
-        GameEventsManager.StopListening(GameEventTypeGameObject.BUY_A_CHAMPION,GenerateChampion);
+        GameEventsManager.StopListening(GameEventTypeChampion.BUY_A_CHAMPION,GenerateChampion);
     }
 }
