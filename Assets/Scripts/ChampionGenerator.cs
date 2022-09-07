55668d3f728b2f8f2d3e81f80ad4124590c44587
Wu <wujiaqu1233@gmail.com>
2022-09-07 02:34:47 +0200

    now the generatechampion func can also generate a champion in deploy quads

--- Assets/Scripts/ChampionGenerator.cs
+++ Assets/Scripts/ChampionGenerator.cs
@@ -6,16 +6,29 @@ public class ChampionGenerator : SingletonManager<ChampionGenerator> {
     protected override void Init() {
         GameEventsManager.StartListening(GameEventTypeChampion.BUY_A_CHAMPION,GenerateChampion);
     }
-    public void GenerateChampion(GameEventTypeChampion ev,Champion _champion) {
+    public void GenerateChampion(GameEventTypeChampion ev,Champion _champion) {//传进来之前已经判断好了,一定有位置生成英雄
         //考虑要不要加限制,目前调试阶段不用,之后写好逻辑应该也还好
-        Dictionary<Vector2,Quad> dict = QuadsManager.Instance.preparationQuadsDict;
-        for (int i = 0; i < dict.Count; i++) {
-            if(dict.ElementAt(i).Value.ChampionOnThisQuad == null) {
+        Dictionary<Vector2,Quad> preparationDict = QuadsManager.Instance.preparationQuadsDict;
+        for (int i = 0; i < preparationDict.Count; i++) {
+            if(preparationDict.ElementAt(i).Value.ChampionOnThisQuad == null) {
                 //说明可以在这里生成
-                Vector3 pos = dict.ElementAt(i).Value.node.worldPosition;
+                Vector3 pos = preparationDict.ElementAt(i).Value.node.worldPosition;
                 GameObject go = Instantiate(_champion.gameObject,pos,Quaternion.identity);
                 if(go.TryGetComponent<Champion>(out Champion champion)) {
-                    champion.OnDeploy(dict.ElementAt(i).Value);
+                    champion.OnDeploy(preparationDict.ElementAt(i).Value);
+                }
+                return;
+            }
+        }
+        //如果场下没有位置了,那就要看看场上
+        Dictionary<Vector2,Quad> deployDict = QuadsManager.Instance.deployQuadsDict;
+        for (int i = 0; i < deployDict.Count; i++) {
+            if(deployDict.ElementAt(i).Value.ChampionOnThisQuad == null) {
+                //说明可以在这里生成
+                Vector3 pos = deployDict.ElementAt(i).Value.node.worldPosition;
+                GameObject go = Instantiate(_champion.gameObject,pos,Quaternion.identity);
+                if(go.TryGetComponent<Champion>(out Champion champion)) {
+                    champion.OnDeploy(deployDict.ElementAt(i).Value);
                 }
                 return;
             }
