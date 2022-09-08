1ab1620e9a79401aa89c75032aeb0dc7fc51dd2d
Wu <wujiaqu1233@gmail.com>
2022-09-08 03:23:34 +0200

    add a property to get champion's name in it's reference

--- Assets/Scripts/CardSO.cs
+++ Assets/Scripts/CardSO.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(menuName = "Cards/Card",fileName = "Card")]
 public class CardSO : ScriptableObject {
-    
+    public string ChampionName {
+        get {
+            Champion champion = null;
+            if(championPrefab.TryGetComponent<Champion>(out champion)) {
+                return champion.ChampionName;
+            }
+            return null;
+        }
+    }
     [SerializeField]
     private int cost;//购买所需费用
     public int Cost => cost;
