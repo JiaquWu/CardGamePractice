11bbb6e7d953dcee668d166d239f1c76913c3fc1
Wu <wujiaqu1233@gmail.com>
2022-09-08 03:31:36 +0200

    implement champion upgrade system with different game state, add a list for storing the champions bought during combat. change some static method to normal method to have an instance in the game scene

--- Assets/Scripts/ChampionManagerBase.cs
+++ Assets/Scripts/ChampionManagerBase.cs
@@ -9,15 +9,23 @@ public class ChampionManagerBase : SingletonManager<ChampionManagerBase> {
     protected static Dictionary<string,List<Champion>> championsDict;
     private static int spaceTakenByChampions;//已经占用的人口数量
     public static int SpaceTakenByChampions => spaceTakenByChampions;//在考虑要不要把这个字段换到其他位置?不确定 再看看
+    private List<Champion> championsBoughtLastCombatRound;//在战斗阶段买的所有英雄都会放到这里面,战斗阶段开始清零,deploy阶段开始的之后会挨个检测是否能合成
     protected override void Init() {
         spaceTakenByChampions = 0;//初始状态下没英雄在上面
+        championsBoughtLastCombatRound = new List<Champion>();
+        GameEventsManager.StartListening(GameEventTypeVoid.ENTER_DEPLOY_STATE,OnEnterDeployState);
+        GameEventsManager.StartListening(GameEventTypeVoid.ENTER_COMBAT_STATE,OnEnterCombatState);
     }
-    static void InitDict() {
+    private void OnDisable() {
+        GameEventsManager.StopListening(GameEventTypeVoid.ENTER_DEPLOY_STATE,OnEnterDeployState);
+        GameEventsManager.StopListening(GameEventTypeVoid.ENTER_COMBAT_STATE,OnEnterCombatState);
+    }
+    void InitDict() {
         if(championsDict == null) {
             championsDict = new Dictionary<string, List<Champion>>();
         }
     }
-    public static void RegisterChampion(Champion champion) {
+    public void RegisterChampion(Champion champion) {
         InitDict();
         if(!championsDict.ContainsKey(champion.ChampionName)) {
             championsDict.Add(champion.ChampionName,new List<Champion>());
@@ -25,32 +33,12 @@ public class ChampionManagerBase : SingletonManager<ChampionManagerBase> {
         championsDict[champion.ChampionName].Add(champion);
         //应该需要遍历一遍这个list,如果有三个0级的,就让第一个0级的callback(1),其他两个callback(0)
         //如果有三个0级的并且有两个1级的,就让第一个1级的callback(2),其余的全部callback(0)
-        if(championsDict[champion.ChampionName].Count <= 2) return;
-        IEnumerable<Champion> championLevel0IE =  
-        from item in championsDict[champion.ChampionName]
-        where item.Level == 0
-        select item;
-        List<Champion> championLevel0 = championLevel0IE.ToList();
-        IEnumerable<Champion> championLevel1IE = 
-        from item in championsDict[champion.ChampionName]
-        where item.Level == 1
-        select item;
-        List<Champion> championLevel1 = championLevel1IE.ToList();
-        if(championLevel0.Count >= 3) {
-            championLevel0[0].OnChampionUpgrade(1);
-            championLevel0[1].OnChampionUpgrade(0);
-            championLevel0[2].OnChampionUpgrade(0);
-            championLevel1.Add(championLevel0[0]);
-            GameEventsManager.TriggerEvent(GameEventTypeInt.CHAMPION_UPGRADE,0);
+        if(GameManager.Instance.PlayState.ActiveState.name == OnPlayState.COMBAT) {
+            Instance.championsBoughtLastCombatRound.Add(champion);//如果在战斗中买的,要加进来
         }
-        if(championLevel1.Count >= 3) {
-            championLevel1[0].OnChampionUpgrade(2);
-            championLevel1[1].OnChampionUpgrade(0);
-            championLevel1[2].OnChampionUpgrade(0);
-            GameEventsManager.TriggerEvent(GameEventTypeInt.CHAMPION_UPGRADE,2);
-        }
+        CheckChampionUpgrade(champion);
     }
-    public static void UnregisterChampion(Champion champion) {
+    public void UnregisterChampion(Champion champion) {
         InitDict();
         if(!championsDict.ContainsKey(champion.ChampionName)) {
             Debug.LogError("no champion instance in this dictionary");
@@ -58,9 +46,73 @@ public class ChampionManagerBase : SingletonManager<ChampionManagerBase> {
             championsDict[champion.ChampionName].Remove(champion);
         }
     }
-    public static void OnSpaceChange(int modifier) {
+    public void OnSpaceChange(int modifier) {//不能写成静态方法,要不然不需要实例
         //如果新英雄进来,那么人口增加,如果有英雄somehow出去,那就减少
         spaceTakenByChampions += modifier;
-        Debug.Log("现在的人口是: " + spaceTakenByChampions);
+    }
+    public static bool CanThisChampionUpgrade(Champion champion) {
+        //给一个英雄进来,看看它能不能买了就升级,这里要判断在不在战斗,不在战斗的话场下场上都可以合成,在的话只能场下合成
+        if(championsDict.ContainsKey(champion.ChampionName)) {
+            //如果字典里面有这个英雄,那还要看看有没有两个一级的
+            int amount = 0;
+            if(GameManager.Instance.PlayState.ActiveState.name == OnPlayState.DEPLOY) {
+                foreach (var item in championsDict[champion.ChampionName]){
+                    if(item.Level == 0) amount ++;
+                }
+            }else if(GameManager.Instance.PlayState.ActiveState.name == OnPlayState.COMBAT) {
+                foreach (var item in championsDict[champion.ChampionName]){
+                    if(item.Level == 0 && item.LastQuadThisChampionStand is PreparationQuad)  amount ++;
+                }
+            }
+            return amount >= 2;//一星卡最多场上有两张
+        }
+        return false;
+    }
+    private static void CheckChampionUpgrade(Champion champion) {
+        if(championsDict[champion.ChampionName].Count <= 2) return;
+        //要分combat还是deploy来判断怎么执行
+        IEnumerable<Champion> championLevel0IE =  GameManager.Instance.PlayState.ActiveState.name == OnPlayState.DEPLOY?//不同的状态,不同的判定条件
+        from item in championsDict[champion.ChampionName]
+        where item.Level == 0
+        select item :
+        GameManager.Instance.PlayState.ActiveState.name == OnPlayState.COMBAT?
+        from item in championsDict[champion.ChampionName]
+        where item.Level == 0 && (item.LastQuadThisChampionStand is PreparationQuad || Instance.championsBoughtLastCombatRound.Contains(item)) 
+        //在战斗状态下,如果是1级并且是刚刚买的champion或者在preparation上面的就可以
+        select item : null;
+        List<Champion> championLevel0 = championLevel0IE.ToList();
+        IEnumerable<Champion> championLevel1IE = GameManager.Instance.PlayState.ActiveState.name == OnPlayState.DEPLOY? //同上
+        from item in championsDict[champion.ChampionName]
+        where item.Level == 1
+        select item : 
+        GameManager.Instance.PlayState.ActiveState.name == OnPlayState.COMBAT?
+        from item in championsDict[champion.ChampionName]
+        where item.Level == 1 && (item.LastQuadThisChampionStand is PreparationQuad || Instance.championsBoughtLastCombatRound.Contains(item))
+        select item : null;
+        List<Champion> championLevel1 = championLevel1IE.ToList();
+        if(championLevel0 != null && championLevel0.Count >= 3) {//意识到这里的3是写死的,如果改变玩法下面的代码都要改,不够通用!
+            championLevel0[0].OnChampionUpgrade(1);
+            championLevel0[1].OnChampionUpgrade(0);
+            championLevel0[2].OnChampionUpgrade(0);
+            championLevel1.Add(championLevel0[0]);
+        }
+        if(championLevel1 != null && championLevel1.Count >= 3) {
+            championLevel1[0].OnChampionUpgrade(2);
+            championLevel1[1].OnChampionUpgrade(0);
+            championLevel1[2].OnChampionUpgrade(0);
+            GameEventsManager.TriggerEvent(GameEventTypeChampion.CHAMPION_UPGRADE_LEVEL_2,championLevel1[0]);
+        }
+    }
+    private void OnEnterCombatState(GameEventTypeVoid ev) {
+        if(championsBoughtLastCombatRound.Count > 0) {//战斗开始的时候清空
+            championsBoughtLastCombatRound.Clear();
+        }
+    }
+    private void OnEnterDeployState(GameEventTypeVoid ev) {//开始买东西之前把每个champion都检查一遍
+        if(championsBoughtLastCombatRound == null && championsBoughtLastCombatRound.Count == 0) return;
+        championsBoughtLastCombatRound.Distinct().ToList();
+        foreach (var item in championsBoughtLastCombatRound) {
+            CheckChampionUpgrade(item);
+        }
     }
 }
