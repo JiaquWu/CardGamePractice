27107a990ca85685b687bc79f8ecb6f8858f9ebf
Wu <wujiaqu1233@gmail.com>
2022-09-10 03:32:44 +0200

    divide generating champion func into 2 funcs, add generateenemychampion func

--- Assets/Scripts/ChampionGenerator.cs
+++ Assets/Scripts/ChampionGenerator.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using System.Linq;
 public class ChampionGenerator : SingletonManager<ChampionGenerator> {
     protected override void Init() {
-        GameEventsManager.StartListening(GameEventTypeChampion.BUY_A_CHAMPION,GenerateChampion);
+        GameEventsManager.StartListening(GameEventTypeChampion.BUY_A_CHAMPION,GenerateAllyChampion);
     }
-    public void GenerateChampion(GameEventTypeChampion ev,Champion _champion) {//传进来之前已经判断好了,一定有位置生成英雄
+    public void GenerateAllyChampion(GameEventTypeChampion ev,Champion _champion) {//传进来之前已经判断好了,一定有位置生成英雄
         //考虑要不要加限制,目前调试阶段不用,之后写好逻辑应该也还好
         Dictionary<Vector2,Quad> preparationDict = QuadsManager.Instance.preparationQuadsDict;
         for (int i = 0; i < preparationDict.Count; i++) {
@@ -15,7 +15,7 @@ public class ChampionGenerator : SingletonManager<ChampionGenerator> {
                 Vector3 pos = preparationDict.ElementAt(i).Value.node.worldPosition;
                 GameObject go = Instantiate(_champion.gameObject,pos,Quaternion.identity);
                 if(go.TryGetComponent<Champion>(out Champion champion)) {
-                    champion.OnDeploy(preparationDict.ElementAt(i).Value);
+                    champion.OnDeploy(preparationDict.ElementAt(i).Value,true);
                 }
                 return;
             }
@@ -28,7 +28,7 @@ public class ChampionGenerator : SingletonManager<ChampionGenerator> {
                 Vector3 pos = deployDict.ElementAt(i).Value.node.worldPosition;
                 GameObject go = Instantiate(_champion.gameObject,pos,Quaternion.identity);
                 if(go.TryGetComponent<Champion>(out Champion champion)) {
-                    champion.OnDeploy(deployDict.ElementAt(i).Value);
+                    champion.OnDeploy(deployDict.ElementAt(i).Value,true);
                 }
                 return;
             }
@@ -36,7 +36,25 @@ public class ChampionGenerator : SingletonManager<ChampionGenerator> {
         //如果没有位置可以生成
         Debug.LogWarning("no place for instantiating a new champion");
     }
+    public void GenerateEnemyChampion(EnemyUnit enemy) {
+        if(QuadsManager.Instance.enemyQuadsDict.ContainsKey(enemy.quadToStayCoordinate)) {
+            Quad quad = QuadsManager.Instance.enemyQuadsDict[enemy.quadToStayCoordinate];
+            Vector3 pos = quad.node.worldPosition;
+            GameObject go = Instantiate(enemy.enemyGameObject,pos,Quaternion.identity);
+            go.transform.rotation = Quaternion.Euler(0,180,0);
+            if(go.TryGetComponent<Champion>(out Champion champion)) {
+                champion.OnDeploy(quad,false,enemy.level);
+            }
+        } 
+        
+    }
+    public void GenerateEnemyChampionsInCurrentRound() {
+        EnemyBuildSO build = GameManager.Instance.GetCurrentEnemyBuild();
+        foreach (var item in build.enmiesInOneTurn) {
+            GenerateEnemyChampion(item);
+        }
+    }
     private void OnDisable() {
-        GameEventsManager.StopListening(GameEventTypeChampion.BUY_A_CHAMPION,GenerateChampion);
+        GameEventsManager.StopListening(GameEventTypeChampion.BUY_A_CHAMPION,GenerateAllyChampion);
     }
 }
