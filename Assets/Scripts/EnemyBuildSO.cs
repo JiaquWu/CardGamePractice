9f8724859cf47c2eb50655da5224c0319940c1e4
Wu <wujiaqu1233@gmail.com>
2022-09-10 03:20:40 +0200

    add EnemyBuildSO, EnemyBuildSequenceSO, and some test SOs to make custom enemy builds

---
+++ Assets/Scripts/EnemyBuildSO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "EnemyBuilds/EnemyBuild",fileName = "EnemyBuild")]
+public class EnemyBuildSO : ScriptableObject {
+    public List<EnemyUnit> enmiesInOneTurn;
+}
+[Serializable]
+public struct EnemyUnit {
+    public GameObject enemyGameObject;
+    public Vector2 quadToStayCoordinate;//从字典中找
+    public int level;//enemy等级,0为1级,2为3级
+}
---
+++ Assets/Scripts/EnemyBuildSequence.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "EnemyBuilds/EnemyBuildSequence",fileName = "EnemyBuildSequence",order = 0)]
+public class EnemyBuildSequence : ScriptableObject {
+    public List<EnemyBuildSO> enemyBuilds;
+    public EnemyBuildSO GetCurrentEnemyBuild(int index) {
+        if(index <= enemyBuilds.Count - 1) {
+            return enemyBuilds[index];
+        }
+        return null;
+    }
+}
